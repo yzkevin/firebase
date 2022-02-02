fac8add1672297b5536aa8cf524bbb605d579477
morgan <moemcd@google.com>
2022-02-02 15:07:53 -0500

    testing spm

--- Package.swift
+++ Package.swift
@@ -152,7 +152,7 @@ let package = Package(
       url: "https://github.com/google/GoogleAppMeasurement.git",
       // Note that CI changes the version to the head of main for CI.
       // See scripts/setup_spm_tests.sh.
-      .exact("8.12.0")
+      .branch("main")
     ),
     .package(
       name: "GoogleDataTransport",
